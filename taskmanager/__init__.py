702b0f2b0283dbe6556b67ebdae7d8db4a1ee648
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-03-29 18:40:37 +0000

    Add two tables for our database

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,4 +10,5 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
+
 from taskmanager import routes #noqa
